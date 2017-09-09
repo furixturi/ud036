6d0cdac5eaba647ebbc222bf4728c15af1d262c7
furixturi <alabebop@gmail.com>
2017-09-09 15:41:09 +0900

    add captain america movie instance and show its trailer

--- lesson-5/movies/entertainment_center.py
+++ lesson-5/movies/entertainment_center.py
@@ -7,11 +7,18 @@ toy_story = media.Movie("Toy Story",
 #           file_name.class_name()
 # Movie class's __init__ function gets called
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+captain_america_3 = media.Movie("Captain America 3: Civil War",
+                                "Political pressure mounts to install a system of accountability when the actions of the Avengers lead to collateral damage. The new status quo deeply divides members of the team. Captain America (Chris Evans) believes superheroes should remain free to defend humanity without government interference. Iron Man (Robert Downey Jr.) sharply disagrees and supports oversight. As the debate escalates into an all-out feud, Black Widow (Scarlett Johansson) and Hawkeye (Jeremy Renner) must pick a side.",
+                                "http://t3.gstatic.com/images?q=tbn:ANd9GcTz1xU3qYlGXViIS51HIQh71D339ceW2nlWnb8zzSaJAL0newVj",
+                                "https://www.youtube.com/watch?v=1L3c17AmCZw")
+captain_america_3.show_trailer()
--- lesson-5/movies/media.py
+++ lesson-5/movies/media.py
@@ -6,11 +6,16 @@ trailer
 
 *self will be automatically added at instantiation
 '''
+import webbrowser
 
 class Movie():
+    
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image = poster_image
         self.trailer = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer)
