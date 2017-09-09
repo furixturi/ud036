a89b233360ff0f472c85f1804f0357e543923ebb
furixturi <alabebop@gmail.com>
2017-09-09 15:26:47 +0900

    instantiate toy_story

--- lesson-5/movies/media.py
+++ lesson-5/movies/media.py
@@ -6,7 +6,8 @@ trailer
 '''
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image = poster_image
