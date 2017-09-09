decc6049658f5b1468ea2af4ea167a40eee2ee31
furixturi <alabebop@gmail.com>
2017-09-09 15:43:01 +0900

    add explanation comments for class construction

--- lesson-5/movies/media.py
+++ lesson-5/movies/media.py
@@ -9,7 +9,7 @@ trailer
 import webbrowser
 
 class Movie():
-    
+    # constructor, being called automatically at media.Motie(title, storyline, poster, trailer)
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
@@ -17,5 +17,6 @@ class Movie():
         self.poster_image = poster_image
         self.trailer = trailer_youtube
 
+    # instance method, self is automatically given 
     def show_trailer(self):
         webbrowser.open(self.trailer)
